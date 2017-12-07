95def7b02055fd6caffefadff719311e8fc62815
timothy Tamm <tamm@college.harvard.edu>
2017-12-06 22:33:41 -0500

    added new tests

--- tests.c
+++ tests.c
@@ -1,9 +1,9 @@
-
 // x should have a full taint from n.
 int test0(int n) {
-  int x = 0;
+  int x = 1;
   while (n > 1) {
     x *= n;
+    n--;
   }
   return x;
 }
@@ -38,3 +38,30 @@ int test4(int a, int b) {
   }
   return x;
 }
+
+struct node {
+  struct node *next;
+  int val;
+};
+
+// x should have a direct taint from a and an indirect taint from b if b <= a OR an indirect taint from a and b if b > a
+int test5(int a, int b) {
+  struct node nodes[a];
+  if (b > a) {
+    return -1;
+  }
+  for (int i = 0; i < b; i++) {
+    nodes[i].val = a;
+    nodes[i].next = &nodes[i+1];
+  }
+  nodes[9].val = a;
+  nodes[0].next = 0;
+  int x = 0;
+  struct node *head = &nodes[0];
+  while (head != 0) {
+    x += head->val;
+    head = head->next;
+  }
+  return x;
+}
+
